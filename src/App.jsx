590af7861e4ef260457e8752667c4a3687d8919d
hrishibpract <hrishikesh@bpract.com>
2026-03-27 15:42:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// Header
+    // -Logo
+    // -Nav items
+// Body
+    // products
+        // - productCards   
+//  Footer
+    //Logo  
+    // Address
+    // other menus
+
+const App = () => {
+    return (
+      <div>
+    
+    </div>
+  )
+}
+
+export default App
