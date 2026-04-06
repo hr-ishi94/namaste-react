ff9c2b8d6765cdc0e2fb25c7f32f8eda8e77e50c
hrishibpract <hrishikesh@bpract.com>
2026-04-06 17:43:05 +0530

    made the products list from api

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,18 @@
 import ProductCard from "./ProductCard";
 import { resList } from "../../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Products = () => {
   const [restaurantList, setRestaurantList] = useState(resList);
+  const fetchData = async ()=>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2541826&lng=75.8370307&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await data.json()
+    console.log(json,'json')
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
      <div className="product-list">
       {restaurantList.map((restaurant,index) => (
