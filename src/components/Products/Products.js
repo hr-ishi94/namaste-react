fef9c2ce06537d6f91b9ad887cbb162911df4643
hrishibpract <hrishikesh@bpract.com>
2026-04-07 18:03:14 +0530

    filter added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,23 +2,28 @@ import ProductCard from "./ProductCard";
 import { resList } from "../../utils/mockData";
 import { useEffect, useState } from "react";
 
-const Products = () => {
-  const [restaurantList, setRestaurantList] = useState(resList);
+const Products = ({restaurantList, setRestaurantList,filteredList,setFilteredList}) => {
+  
   const fetchData = async ()=>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2541826&lng=75.8370307&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-    const json = await data.json()
-    console.log(json,'json')
+    const dataList = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2541826&lng=75.8370307&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const {data}= await dataList.json()
+    setRestaurantList(data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setFilteredList(data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   }
 
   useEffect(()=>{
     fetchData()
   },[])
   return (
+    <>
+    {filteredList.length===0?<div>Loading....</div>:
      <div className="product-list">
-      {restaurantList.map((restaurant,index) => (
+      {filteredList.map((restaurant,index) => (
         <ProductCard key={index} restaurant={restaurant} />
       ))}
-    </div>
+      </div>
+    }
+    </>
   );
 };
 
